808a55e40eb7f497180cf081a07b642dbd7525b9
Tigran <tigranma@gmail.com>
2023-04-17 20:13:02 +0300

    BottomNavigation

--- bottomnavigationlesson/src/main/java/com/example/bottomnavigationlesson/bottom_navigation/NavGraph.kt
+++ bottomnavigationlesson/src/main/java/com/example/bottomnavigationlesson/bottom_navigation/NavGraph.kt
@@ -1,7 +1,6 @@
 package com.example.bottomnavigationlesson.bottom_navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
