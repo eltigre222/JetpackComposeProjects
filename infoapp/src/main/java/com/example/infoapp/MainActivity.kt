3d40d796b80f41822938da964f89ceac099bbcf4
Tigran <tigranma@gmail.com>
2023-05-04 22:48:04 +0300

    InfoApp - topAppBar

---
+++ infoapp/src/main/java/com/example/infoapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.infoapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.example.infoapp.ui.theme.JetpackComposeProjectsTheme
+import com.example.infoapp.ui_components.MainTopBar
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val scaffoldState = rememberScaffoldState()
+            val topBarTitle = remember{
+                mutableStateOf("Spain")
+            }
+            JetpackComposeProjectsTheme {
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        MainTopBar(
+                            title = topBarTitle.value,
+                            scaffoldState
+                        )
+                    }
+                ) {
+
+                }
+            }
+        }
+    }
+}
