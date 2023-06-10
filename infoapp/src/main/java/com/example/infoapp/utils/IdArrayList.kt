7816970e3662efa8a8b3b85210f337e5f498672f
TigranMonte <tigranma@gmail.com>
2023-06-10 16:16:39 +0300

    App update

---
+++ infoapp/src/main/java/com/example/infoapp/utils/IdArrayList.kt
@@ -0,0 +1,11 @@
+package com.example.infoapp.utils
+
+import com.example.infoapp.R
+
+object IdArrayList {
+    val listId = listOf(
+        R.array.england,
+        R.array.spain,
+        R.array.italy
+    )
+}
---
+++ infoapp/src/main/java/com/example/infoapp/utils/ListItem.kt
@@ -0,0 +1,6 @@
+package com.example.infoapp.utils
+
+data class ListItem(
+    val title: String,
+    val imageName: String
+)
