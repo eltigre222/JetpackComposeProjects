7a71309ef9f5bae34560cc970f2242506f7b1604
TigranMonte <tigranma@gmail.com>
2023-11-20 23:06:06 +0300

    ConstraintLayout in Compose

--- constraintlayoutlesson/src/main/java/com/example/constraintlayoutlesson/MainActivity.kt
+++ constraintlayoutlesson/src/main/java/com/example/constraintlayoutlesson/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
--- singlelinechart/src/main/java/com/example/singlelinechart/MainActivity.kt
+++ singlelinechart/src/main/java/com/example/singlelinechart/MainActivity.kt
@@ -30,7 +30,6 @@ import com.example.singlelinechart.ui.theme.JetpackComposeProjectsTheme
 import kotlin.random.Random
 
 const val steps = 10
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -82,6 +81,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
     fun getPointsList(): List<Point> {
         val list = ArrayList<Point>()
         for (i in 0..30) {
@@ -94,6 +94,7 @@ class MainActivity : ComponentActivity() {
         }
         return list
     }
+
     private fun getMax(list: List<Point>): Float{
         var max = 0F
         list.forEach { point ->
@@ -101,6 +102,7 @@ class MainActivity : ComponentActivity() {
         }
         return max
     }
+
     private fun getMin(list: List<Point>): Float{
         var min = 100F
         list.forEach { point ->
