95bb293300f99a3171071ca43be1f5de47103def
TigranMonte <tigranma@gmail.com>
2024-02-04 20:10:17 +0300

    QR code scanner

--- qrcodescan/src/main/java/com/example/qrcodescan/MainActivity.kt
+++ qrcodescan/src/main/java/com/example/qrcodescan/MainActivity.kt
@@ -5,43 +5,87 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.qrcodescan.data.MainDb
+import com.example.qrcodescan.data.Product
 import com.example.qrcodescan.ui.theme.JetpackComposeProjectsTheme
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
     lateinit var mainDb: MainDb
+    var counter = 0
 
     private val scanLauncher = registerForActivityResult(ScanContract()) { result ->
         if (result.contents == null) {
-            
+
         } else {
-             Toast.makeText(this, "Scan data: ${result.contents}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Scan data: ${result.contents}", Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val productStateList = mainDb.dao.getAllProducts()
+                .collectAsState(initial = emptyList())
+
+            val coroutineScope = rememberCoroutineScope()
+
             JetpackComposeProjectsTheme {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                Column(
+                    Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.9f),
+                    ) {
+                        items(productStateList.value) { product ->
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = product.name,
+                                textAlign = TextAlign.Center
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                        }
+                    }
                     Button(onClick = {
-                        scan()
+                        coroutineScope.launch {
+                            mainDb.dao.insertProduct(
+                                Product(
+                                    null,
+                                    "Product ${counter++}",
+                                    "asdkgutpw" 
+                                )
+                            )
+                        }
                     }) {
-                        Text(text = "Scan")
+                        Text(text = "Create data")
                     }
                 }
             }
