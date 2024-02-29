80ef25ec1dfe8ab3488d9778e26f3f3e76af9d01
TigranMonte <tigranma@gmail.com>
2024-02-29 21:55:19 +0300

    QR code scanner

--- qrcodescan/src/main/java/com/example/qrcodescan/MainActivity.kt
+++ qrcodescan/src/main/java/com/example/qrcodescan/MainActivity.kt
@@ -146,15 +146,12 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
     private fun scan() {
         scanLauncher.launch(getScanOptions())
     }
-
     private fun scanCheck() {
         scanCheckLauncher.launch(getScanOptions())
     }
-
     private fun getScanOptions(): ScanOptions {
         return ScanOptions().apply {
             setDesiredBarcodeFormats(ScanOptions.QR_CODE)
